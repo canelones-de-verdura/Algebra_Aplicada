3ddc8d90ce119f03e083472598f2c6dd6d86f1bb
Eze <eze.gonzalez@vera.com.uy>
2025-11-13 12:30:41 -0300

    .gitignore

--- Generar_Matriz.py
+++ Generar_Matriz.py
@@ -3,4 +3,6 @@ import numpy as np
 def generar_matriz_identidad(n):
     matriz_aleatoria = np.random.randint(0, 2, size=(n, n))
     return matriz_aleatoria
-print(generar_matriz_identidad(3))
+
+if __name__ == "__main__":
+    print(generar_matriz_identidad(3))
